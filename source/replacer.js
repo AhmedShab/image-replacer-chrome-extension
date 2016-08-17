d52cf1ffbf4b966e979996153ac19df125584404
AhmedShab <ahmed.vuw@gmail.com>
2016-08-17 20:37:15 +1200

    adding the the neccessery files to run the extention

---
+++ source/replacer.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=troll",
+    success: function(result) {
+        image_url = result.data.image_url;
+				$("body").append(
+					`<div class='box'><img width=500 src="${image_url}"></div>`
+				);
+    }
+});
+
+// $("div").text("Hello world!");
